f54cc126d4858e370aad0ed3a21ec8180702a88b
klishchov-bohdan <bogdan.bogdan2525@gmail.com>
2022-02-04 16:45:28 +0200

    added tokens test

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
+	github.com/stretchr/testify v1.7.0
 	github.com/swaggo/http-swagger v1.2.0
 	github.com/swaggo/swag v1.7.8
 	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838
@@ -17,16 +18,19 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
--- internal/tests/tokens_test.go
+++ internal/tests/tokens_test.go
@@ -1,9 +1,96 @@
 package tests
 
 import (
+	"github.com/google/uuid"
+	"github.com/klishchov-bohdan/delivery/config"
+	"github.com/klishchov-bohdan/delivery/internal/tests/helper"
+	"github.com/klishchov-bohdan/delivery/internal/token"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"testing"
 )
 
 type TokensTestSuite struct {
 	suite.Suite
+	cfg *config.Config
+}
+
+func (ts *TokensTestSuite) SetupSuite() {
+	ts.cfg = &config.Config{
+		AccessSecret:         "access_secret",
+		RefreshSecret:        "refresh_secret",
+		AccessTokenLifeTime:  1,
+		RefreshTokenLifeTime: 2,
+	}
+}
+
+func TestTokenService(t *testing.T) {
+	suite.Run(t, new(TokensTestSuite))
+}
+
+func (ts *TokensTestSuite) TestGetTokenFromBearerString() {
+	testCases := []helper.TestCaseGetBearerString{
+		{
+			Name:         "Get token successful",
+			BearerString: "Bearer djvnrpenvdskjvnawpefo.aewoivnfpaeoghaierfurea.aurehfpaurfiaur",
+			Expected:     "djvnrpenvdskjvnawpefo.aewoivnfpaeoghaierfurea.aurehfpaurfiaur",
+		},
+		{
+			Name:         "Get empty token from incorrect string",
+			BearerString: "Bearerdjvnrpenvdskjvnawpefo.aewoivnfpaeoghaierfurea.aurehfpaurfiaur",
+			Expected:     "",
+		},
+	}
+
+	for _, testCase := range testCases {
+		ts.T().Run(testCase.Name, func(t *testing.T) {
+			actual := token.GetTokenFromBearerString(testCase.BearerString)
+			assert.Equal(t, testCase.Expected, actual)
+		})
+	}
+}
+
+func (ts *TokensTestSuite) TestValidateAccessToken() {
+	// preparation
+	userID := uuid.New()
+	accessString, _ := token.GenerateToken(userID, ts.cfg.AccessTokenLifeTime, ts.cfg.AccessSecret)
+	accessStringWithRefreshSecret, _ := token.GenerateToken(userID, ts.cfg.AccessTokenLifeTime, ts.cfg.RefreshSecret)
+	accessExpiredString, _ := token.GenerateToken(userID, -1, ts.cfg.AccessSecret)
+	testCases := []helper.TestCaseValidateToken{
+		{
+			Name:            "Valid access token string",
+			TokenString:     accessString,
+			IsValidExpected: true,
+		},
+		{
+			Name:            "Invalid access token string",
+			TokenString:     accessString + "hello_world",
+			IsValidExpected: false,
+		},
+		{
+			Name:            "Valid access token signed with refresh secret",
+			TokenString:     accessStringWithRefreshSecret,
+			IsValidExpected: false,
+		},
+		{
+			Name:            "Expired access token",
+			TokenString:     accessExpiredString,
+			IsValidExpected: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		ts.T().Run(testCase.Name, func(t *testing.T) {
+			isValid, _ := token.ValidateToken(testCase.TokenString, ts.cfg.AccessSecret)
+			claims, _ := token.GetClaims(testCase.TokenString, ts.cfg.AccessSecret)
+			if testCase.IsValidExpected {
+				assert.True(t, isValid)
+				assert.NotNil(t, claims)
+				assert.Equal(t, userID, claims.ID)
+			} else {
+				assert.False(t, isValid)
+				assert.Nil(t, claims)
+			}
+		})
+	}
 }
